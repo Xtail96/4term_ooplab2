0e63c285aeaf42f5078b029770cb8efca92b9622
Xtail <xtail1996@yandex.ru>
2017-05-15 18:10:16 +0300

    Окружность задается от -M_PI до M_PI

--- src/round.h
+++ src/round.h
@@ -6,7 +6,7 @@ class Round : public Arc
 {
 public:
     Round(double x, double y, double radius, double angle = 0, double scale = 1, Color color = Color(0, 0, 0))
-        : Arc(x, y, 0, 2*M_PI, radius, angle, scale, color){}
+        : Arc(x, y, -M_PI, M_PI, radius, angle, scale, color){}
     virtual ~Round(){}
 
     double getPerimetr() const override;
